c31f8efa92d336ef0bb86a7b4482c0c23a2852c6
mateudu <mateudu@outlook.com>
2019-10-23 10:27:26 +0200

    Modify install script

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
-curl -L http://cpanmin.us | sudo perl - App::cpanminus
-sudo cpanm AZURE::AD::AUTH
-sudo cpanm JSON
-sudo cpanm Tk::GridColumns
+apt-get install build-essential
+apt-get install curl
+curl -L http://cpanmin.us | perl - App::cpanminus
+cpanm LWP::UserAgent
+cpanm JSON::MaybeXS
+cpanm Azure::AD::Auth
+cpanm JSON
+
+# Install Tk
+apt-get install libx11-dev
+apt-get install perl-tk
+cpanm Tk::GridColumns
